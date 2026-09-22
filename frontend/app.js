e6269a7f3d8a4069a88d885e950fbcbcf414dd5c
avo-offspring <utsaikhateek@gmail.com>
2026-09-22 12:12:39 +0200

    revert previous changes as orderid was already converted to string by line 26 "body: JSON.stringify({ orderId: orderId, itemName: itemName })".

--- frontend/app.js
+++ frontend/app.js
@@ -9,7 +9,6 @@ async function submitOrder() {
     // 2. Note: Generate a unique ID on the client side 
     // to use as our DynamoDB Partition Key (Primary Key).
     const orderIdentity= "ORD-" + Math.floor(Math.random() * 10000);
-    const orderId = orderId.toString();
     const msgField = document.getElementById('msg');
 
     // Inform user the data is traveling to the cloud
