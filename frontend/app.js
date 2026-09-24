5e24ed8dae86ac2d3b984f967abd23956da23e42
avo-offspring <utsaikhateek@gmail.com>
2026-09-24 21:27:06 +0200

    Adding the new API URL

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,6 @@
 // CONCEPT: This endpoint represents your API Gateway entry point.
 // You must replace this placeholder string with your real Invoke URL in Phase 5!
-const API_URL = "https://cil6wpo2jk.execute-api.us-east-1.amazonaws.com/orders";
+const API_URL = "https://6syridi1s7.execute-api.us-east-1.amazonaws.com/prod/orders";
 
 async function submitOrder() {
     // 1. Fetch values from the HTML input fields
